f14c8103721fae249b5fb0e9391d228201a4f9a6
José Rafael Ramón Navarro <jrrn@MacBook-Pro-de-Jose.local>
2018-10-22 21:47:41 +0200

    No Solid Calc

--- CalcToSolid/NoSolidCalc.cs
+++ CalcToSolid/NoSolidCalc.cs
@@ -3,8 +3,38 @@ namespace CalcToSolid
 {
     public class NoSolidCalc
     {
-        public NoSolidCalc()
+
+        public int Operation(OperationType operationType, string operand1, string operand2)
         {
+
+            var parsed1 =  int.TryParse(operand1, out int operandOneParsed);
+            var parsed2 = int.TryParse(operand2, out int operandTwoParsed);
+
+            if(parsed1 && parsed2){
+                if (operationType.Equals(OperationType.DIV))
+                {
+                    return operandOneParsed / operandTwoParsed;
+                }
+
+                if (operationType.Equals(OperationType.MUL))
+                {
+                    return operandOneParsed * operandTwoParsed;
+                }
+                if (operationType.Equals(OperationType.SUB))
+                {
+                    return operandOneParsed - operandTwoParsed;
+                }
+
+                return operandOneParsed + operandTwoParsed;
+            }
+            return 0;
         }
+    }
+
+    public enum OperationType {
+        SUM,
+        SUB,
+        DIV,
+        MUL
     }
 }
--- CalcToSolid/Program.cs
+++ CalcToSolid/Program.cs
@@ -6,7 +6,25 @@ namespace CalcToSolid
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            NoSolidOperation();
+        }
+
+        private static void NoSolidOperation(){
+            Console.WriteLine("Intro First Operand");
+            var operand1 = Console.ReadLine();
+
+            Console.WriteLine("Intro Second Operand");
+            var operand2 = Console.ReadLine();
+
+            Console.WriteLine("Intro Operation");
+            Console.WriteLine("0: SUM");
+            Console.WriteLine("1: SUB");
+            Console.WriteLine("2: DIV");
+            Console.WriteLine("3: MUL");
+            var operation = (OperationType)Console.Read();
+
+            var result = new NoSolidCalc().Operation(operation, operand1, operand2);
+            Console.WriteLine(result);
         }
     }
 }
