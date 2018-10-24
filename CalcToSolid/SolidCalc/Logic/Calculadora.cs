326d7ab675d4a5fa4ef0fe5a0dfdafc03cc5808e
J.Rafa Ramón <rramon@plainconcepts.com>
2018-10-24 12:16:44 +0200

    renaming

--- CalcToSolid/SolidCalc/Logic/Calculadora.cs
+++ CalcToSolid/SolidCalc/Logic/Calculadora.cs
@@ -4,20 +4,63 @@ namespace CalcToSolid.SolidCalc.Logic
 {
     public class Calculadora : ICalculadora
     {
-        private static readonly IOperation _sum = new OperationSum(); //Interface segregation
-        private static readonly IOperation _mul = new OperationMul(); //Interface segregation
-        private static readonly IOperation _sub = new OperationSub(); //Interface segregation
-        private static readonly IOperation _div = new OperationDiv(); //Interface segregation
-        private static readonly IOperation _nExpM = new OperationNExpM(); //Interface segregation
+        private readonly IOperation _sumatory;
+        private readonly IOperation _multiplication;
+        private readonly IOperation _substraction;
+        private readonly IOperation _division;
+        private readonly IOperation _exponential;
+
+        public Calculadora()
+        {
+            _sumatory = new OperationSum(); //Interface segregation
+            _multiplication = new OperationMul(); //Interface segregation
+            _substraction = new OperationSub(); //Interface segregation
+            _division = new OperationDiv(); //Interface segregation
+            _exponential = new OperationNExpM(); //Interface segregation
+        }
 
         public int DoOperation(OperationType operation, int operand1, int operand2)
         {
-            if (operation.Equals(OperationType.Div)) return _div.Execute(operand1, operand2);
-            if (operation.Equals(OperationType.Mul)) return _mul.Execute(operand1, operand2);
-            if (operation.Equals(OperationType.Sub)) return _sub.Execute(operand1, operand2);
-            if (operation.Equals(OperationType.Sum)) return _sum.Execute(operand1, operand2);
-            if (operation.Equals(OperationType.NExpM)) return _nExpM.Execute(operand1, operand2);
-            return 0;
+            switch (operation)
+            {
+                case OperationType.Div:
+                    return Division(operand1, operand2);
+                case OperationType.Mul:
+                    return Multiplication(operand1, operand2);
+                case OperationType.Sub:
+                    return Substraction(operand1, operand2);
+                case OperationType.Sum:
+                    return Sumatory(operand1, operand2);
+                case OperationType.NExpM:
+                    return Exponential(operand1, operand2);
+                default:
+                    return 0;
+            }
+        }
+
+        private int Division(int operand1, int operand2)
+        {
+            return _division.Execute(operand1, operand2);
+        }
+
+        private int Multiplication(int operand1, int operand2)
+        {
+            return _multiplication.Execute(operand1, operand2);
+        }
+
+        private int Substraction(int operand1, int operand2)
+        {
+            return _substraction.Execute(operand1, operand2);
+        }
+
+        private int Sumatory(int operand1, int operand2)
+        {
+            return _sumatory.Execute(operand1, operand2);
+        }
+
+        private int Exponential(int operand1, int operand2)
+        {
+            return _exponential.Execute(operand1, operand2);
         }
     }
 }
